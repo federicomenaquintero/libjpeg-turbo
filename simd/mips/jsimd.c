08c1399b6c24bed81e059a53c60b89e567d12759
Federico Mena Quintero <federico@gnome.org>
2020-05-07 17:59:26 -0500

    Pass the sample_range_limit instead of the whole cinfo to the idct functions
    The only thing out of the cinfo that the idct functions ever use
    is the sample_range_limit, or its subset IDCT_range_limit(cinfo).
    
    We split that macro into two:
    
    This is so that the existing code which already uses IDCT_range_limit() can remain unchanged.
    
    We pass the full array sample_range_limit to the idct functions.
    Most of them use the subset and extract it with the first macro,
    but jpeg_idct_float uses the full array.
    
    This removes the last use of cinfo in the simd/ directory.

--- simd/mips/jsimd.c
+++ simd/mips/jsimd.c
@@ -966,7 +966,7 @@ jsimd_can_idct_12x12(void)
 }
 
 GLOBAL(void)
-jsimd_idct_2x2(j_decompress_ptr cinfo, jpeg_component_info *compptr,
+jsimd_idct_2x2(JSAMPLE *sample_range_limit, jpeg_component_info *compptr,
                JCOEFPTR coef_block, JSAMPARRAY output_buf,
                JDIMENSION output_col)
 {
@@ -974,7 +974,7 @@ jsimd_idct_2x2(j_decompress_ptr cinfo, jpeg_component_info *compptr,
 }
 
 GLOBAL(void)
-jsimd_idct_4x4(j_decompress_ptr cinfo, jpeg_component_info *compptr,
+jsimd_idct_4x4(JSAMPLE *sample_range_limit, jpeg_component_info *compptr,
                JCOEFPTR coef_block, JSAMPARRAY output_buf,
                JDIMENSION output_col)
 {
@@ -985,7 +985,7 @@ jsimd_idct_4x4(j_decompress_ptr cinfo, jpeg_component_info *compptr,
 }
 
 GLOBAL(void)
-jsimd_idct_6x6(j_decompress_ptr cinfo, jpeg_component_info *compptr,
+jsimd_idct_6x6(JSAMPLE *sample_range_limit, jpeg_component_info *compptr,
                JCOEFPTR coef_block, JSAMPARRAY output_buf,
                JDIMENSION output_col)
 {
@@ -993,7 +993,7 @@ jsimd_idct_6x6(j_decompress_ptr cinfo, jpeg_component_info *compptr,
 }
 
 GLOBAL(void)
-jsimd_idct_12x12(j_decompress_ptr cinfo, jpeg_component_info *compptr,
+jsimd_idct_12x12(JSAMPLE *sample_range_limit, jpeg_component_info *compptr,
                  JCOEFPTR coef_block, JSAMPARRAY output_buf,
                  JDIMENSION output_col)
 {
@@ -1074,7 +1074,7 @@ jsimd_can_idct_float(void)
 }
 
 GLOBAL(void)
-jsimd_idct_islow(j_decompress_ptr cinfo, jpeg_component_info *compptr,
+jsimd_idct_islow(JSAMPLE *sample_range_limit, jpeg_component_info *compptr,
                  JCOEFPTR coef_block, JSAMPARRAY output_buf,
                  JDIMENSION output_col)
 {
@@ -1090,11 +1090,11 @@ jsimd_idct_islow(j_decompress_ptr cinfo, jpeg_component_info *compptr,
   };
 
   jsimd_idct_islow_dspr2(coef_block, compptr->dct_table, output,
-                         IDCT_range_limit(cinfo));
+                         IDCT_range_limit_from_samples(sample_range_limit));
 }
 
 GLOBAL(void)
-jsimd_idct_ifast(j_decompress_ptr cinfo, jpeg_component_info *compptr,
+jsimd_idct_ifast(JSAMPLE *sample_range_limit, jpeg_component_info *compptr,
                  JCOEFPTR coef_block, JSAMPARRAY output_buf,
                  JDIMENSION output_col)
 {
@@ -1119,7 +1119,7 @@ jsimd_idct_ifast(j_decompress_ptr cinfo, jpeg_component_info *compptr,
 }
 
 GLOBAL(void)
-jsimd_idct_float(j_decompress_ptr cinfo, jpeg_component_info *compptr,
+jsimd_idct_float(JSAMPLE *sample_range_limit, jpeg_component_info *compptr,
                  JCOEFPTR coef_block, JSAMPARRAY output_buf,
                  JDIMENSION output_col)
 {
